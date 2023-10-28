2975919d908db29a8da0d3051733d2db51c198d9
Amirarsalan <amirzangene16@gmail.com>
2023-10-29 02:41:44 +0330

    Finish

---
+++ index.js
@@ -0,0 +1,12 @@
+let charge = 20;
+let locait = "Dubai";
+
+if (
+  (locait === "vancover" || locait === "Dubai" || locait === "California") &&
+  charge < 80 &&
+  charge > 15
+) {
+  console.log("Your device is ready to update");
+} else {
+  console.log("Your can not to update");
+}
